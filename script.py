9400b21fd0ba26c7c8178807102727ea15d091cc
imConnor <54060984+imConnorrr@users.noreply.github.com>
2020-03-27 03:41:26 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,84 @@
 import os
+import zipfile
 
-proxy = 'http://4d7eb21cc7a5a44f865362fd7276f04a:5b0bd2a34a918a3cb2c360cd3d3a4c4c@199.189.86.111:9500'
+from selenium import webdriver
 
-os.environ['http_proxy'] = proxy
-os.environ['HTTP_PROXY'] = proxy
-os.environ['https_proxy'] = proxy
-os.environ['HTTPS_PROXY'] = proxy
+PROXY_HOST = '199.189.86.111'  # rotating proxy
+PROXY_PORT = 9500
+PROXY_USER = '4d7eb21cc7a5a44f865362fd7276f04a'
+PROXY_PASS = '5b0bd2a34a918a3cb2c360cd3d3a4c4c'
 
-from urllib.request import urlopen
-html = urlopen("http://ipv4.icanhazip.com/").read()
-print(html)
+
+manifest_json = """
+{
+    "version": "1.0.0",
+    "manifest_version": 2,
+    "name": "Chrome Proxy",
+    "permissions": [
+        "proxy",
+        "tabs",
+        "unlimitedStorage",
+        "storage",
+        "<all_urls>",
+        "webRequest",
+        "webRequestBlocking"
+    ],
+    "background": {
+        "scripts": ["background.js"]
+    },
+    "minimum_chrome_version":"22.0.0"
+}
+"""
+
+background_js = """
+var config = {
+        mode: "fixed_servers",
+        rules: {
+          singleProxy: {
+            scheme: "http",
+            host: "%s",
+            port: parseInt(%s)
+          },
+          bypassList: ["localhost"]
+        }
+      };
+
+chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+function callbackFn(details) {
+    return {
+        authCredentials: {
+            username: "%s",
+            password: "%s"
+        }
+    };
+}
+
+chrome.webRequest.onAuthRequired.addListener(
+            callbackFn,
+            {urls: ["<all_urls>"]},
+            ['blocking']
+);
+""" % (PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS)
+
+
+def get_chromedriver(use_proxy=False, user_agent=None):
+    path = os.path.dirname(os.path.abspath(__file__))
+    chrome_options = webdriver.ChromeOptions()
+    if use_proxy:
+        pluginfile = 'proxy_auth_plugin.zip'
+
+        with zipfile.ZipFile(pluginfile, 'w') as zp:
+            zp.writestr("manifest.json", manifest_json)
+            zp.writestr("background.js", background_js)
+        chrome_options.add_extension(pluginfile)
+    if user_agent:
+        chrome_options.add_argument('--user-agent=%s' % user_agent)
+    driver = webdriver.Chrome(
+        os.path.join(path, '/usr/bin/chromedriver'),
+        chrome_options=chrome_options)
+    return driver
+
+driver = get_chromedriver(use_proxy=True)
+#driver.get('https://www.google.com/search?q=my+ip+address')
+driver.get('http://ipv4.icanhazip.com')
